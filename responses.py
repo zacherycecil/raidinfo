476baca5e8c13864a74a5700c8f8762bb1edc59a
Zachery <zcjl98@yahoo.ca>
2023-05-11 14:15:28 -0300

    cleaned up the code and added basic logging

--- responses.py
+++ responses.py
@@ -1,49 +1,82 @@
 import os
 from dotenv import load_dotenv
 import sqlite3
+import logging as log
+import datetime
+
+log.basicConfig(filename='commands.log', encoding='utf-8', level=log.DEBUG)
 
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-pkmntypes = ['Normal','Fire','Water','Grass','Electric','Ice','Fighting','Poison','Ground','Flying','Psychic','Bug','Rock','Ghost','Dark','Dragon','Steel','Fairy']
-
-def handle_response(message : str) -> str:
+def handle_response(author : str, message : str) -> str:
     
-    p_message = message.lower()
+    args = message.lower().split()
 
-    args = p_message.split()
     if args[0] == '!raid':
+        log_command(author, message)
         return raid(args)
+    
     if args[0] == '!info':
+        log_command(author, message)
         return 'Hello, I am a raid information bot created by Hail. Try doing a command like "!raid <pokemon name>". Examples:\n*!raid clodsire*\n*!raid clodsire EBU26B dark*'
 
 def raid(args):
     try:
         conn = sqlite3.connect('raidinfo.db')
-        if len(args) == 1:
+        if len(args) == 1: # !raid didnt specify any details
             return 'Please specify a pokemon name, or name + code + type. Examples:\n*!raid clodsire*\n*!raid clodsire EBU26B dark*'
+        
         else:
             msg = ''
             cursor = conn.execute("SELECT NAME, MOVES, HERBA from SIXSTAR;")
-            for row in cursor:
-                if args[1] == row[0].lower(): # pokemon name match found in db
-                    if len(args) == 4 and len(args[2]) == 6: # code specified as third arg
 
-                        # check type validity
-                        typevalid = False
-                        for pkmntype in pkmntypes:
-                            if pkmntype.lower() == args[3].lower():
-                                typevalid = True
+            for pokemon in cursor:
+                pokemon_name = args[1].upper()
 
-                        # raid code and type specified
-                        if typevalid:
-                            msg += 'Please join my raid! Code: **' + args[2].upper() + '**\nTera type **' + args[3].title() + '**\n'
+                if pokemon_name == pokemon[0].upper(): # pokemon name match found in db
 
-                    # print pokemon info
-                    msg  += '***' + row[0].upper() + '*** (Herba chance: ' + row[2] + ')\n**Moves:**' + row[1] 
-            if(msg != ''):
+                    herba_chance = pokemon[2]
+                    moves = pokemon[1] 
+
+                    if len(args) == 4: # if code and tera type are given
+                        
+                        code = args[2].upper()      # example: EKG45T
+                        tera_type = args[3].title() # example: Dark
+
+                        # if code and tera type are valid
+                        if validate_code(code) and validate_tera_type(tera_type): 
+                            msg += 'Please join my raid! Code: **' + code + '**\nTera type **' + tera_type + '**\n'
+
+                    elif len(args) > 2:
+                        log.warning('Invalid args beyond Pokemon name. Completing request with Pokemon name only.')
+
+                    msg  += '***' + pokemon_name + '*** (Herba chance: ' + herba_chance + ')\n**Moves:**' + moves
+
+            if msg != '':
                 return msg
+            else:
+                log.warning('No pokemon with this name found.')
         
     finally:
         conn.close()
+
+def validate_code(code):
+    if(len(code) != 6):
+        log.warning('Code should be 6 characters in length.')
+
+    return len(code) == 6
+
+def validate_tera_type(tera_type):
+    pkmntypes = ['Normal','Fire','Water','Grass','Electric','Ice','Fighting','Poison','Ground','Flying','Psychic','Bug','Rock','Ghost','Dark','Dragon','Steel','Fairy']
+
+    for pkmntype in pkmntypes:
+        if pkmntype.lower() == tera_type.lower():
+            return True
+        
+    log.warning(tera_type + ' does not appear in the list of valid types.')
+    return False
+
+def log_command(author : str, message : str):
+    log.info('[' + str(datetime.datetime.now()) + '] ' + author + ': ' + message)
